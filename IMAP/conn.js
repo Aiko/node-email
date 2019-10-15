562af6bf2619c245c5d3a4c904b6237deb029952
Priansh Shah <shah@priansh.com>
2019-10-15 03:45:39 +0100

    big refactor & some bugfixes

--- IMAP/conn.js
+++ IMAP/conn.js
@@ -2,6 +2,32 @@ const tls = require('tls')
 const hypertext = require('html-to-text')
 const atob = require('atob')
 const MailParser = require("mailparser-mit").MailParser;
+const assert = require('assert')
+
+const process_emails = (emails, start, stop) => emails
+    .split(/(?=\* [0-9]* FETCH .*(\r\n|\n))/g)
+    .filter(_ => _.length > 5)
+    .map(async email => {
+        const parser = new MailParser()
+        const s = new Promise((s, j) => parser.on('end', mail => s(mail)))
+        parser.write(email)
+        parser.end()
+        const parsed = await s
+
+        if (parsed.headers) {
+            const headers = Object.keys(parsed.headers)
+            const hasHeader = h => headers.filter(header => header.includes(h)).length > 0
+            parsed.headers.seen = hasHeader('\\seen')
+            parsed.headers.starred = hasHeader('\\flagged')
+            try {
+                parsed.headers.id = eval(headers.filter(header => /\* [0-9]* fetch/g.test(header))[0].split(' ')[1])
+            }
+            catch (e) {
+                parsed.headers.id = ('*' != stop ? stop : start)
+            }
+        }
+        return parsed
+    })
 
 class IMAP {
     constructor(options) {
@@ -11,33 +37,39 @@ class IMAP {
         this.queue = {}
         this.buffer = ''
     }
-    async close() { this.sock.destroy() }
-    async open(h, p, u, s, x) {
+    async close() {
+        this.sock.destroy()
+    }
+    async open(host, port, user, pass, xoauth) {
         return await new Promise((s, j) => {
-            let timeout = true
+
+            let wait_for_connect = true
+
             if (this.sock) this.sock.destroy()
             this.sock = tls.connect({
-                host: h || this.opts.host || null,
-                port: p || this.opts.port || 993,
-                user: u || this.opts.user || null,
-                pass: s || this.opts.pass || null
-            }, () => {
-                timeout = false
-                s(true)
-            })
+                host: host || this.opts.host || null,
+                port: port || this.opts.port || 993,
+                user: user || this.opts.user || null,
+                pass: pass || this.opts.pass || null
+            }, () => s(!(wait_for_connect = false)))
+
             this.sock.setEncoding('utf8')
-            var _this = this
-            this.sock.on('data', (d) => {
-                if (/((\n[0-9]{8})|(^[0-9]{8}))(?= (OK|NO))/g.exec(d)) {
-                    let output = _this.buffer
+            const _this = this
+
+            this.sock.on('data', d => {
+                const r_ok = /((\n[0-9]{8})|(^[0-9]{8}))(?= (OK|NO))/gim
+                const s = r_ok.exec(d)
+                if (s) {
+                    // clear the buffer and call corresponding waiter
+                    const output = _this.buffer
                     _this.buffer = ''
-                    _this.queue[/((\n[0-9]{8})|(^[0-9]{8}))(?= (OK|NO))/g.exec(d)[0].trim()]((output + d).trim())                      } else _this.buffer += d
+                    const i = s[0].trim()
+                    _this.queue[i]((output + d).trim())
+                } else _this.buffer += d
             })
+
             setTimeout(() => {
-                if (timeout) {
-                    j('timeout')
-                    this.sock.destroy()
-                }
+                if (wait_for_connect) j(true);
             }, this.opts.timeout || 150000)
         })
     }
@@ -45,42 +77,37 @@ class IMAP {
         return `${n}`.padStart(8, '0')
     }
     async execute(command) {
-        let n = this.line
-        this.line += 1
-        return await new Promise((s, j) => {
-            while (this.queue[this.padLine(n)]) n += 1
-            this.queue[this.padLine(n)] = s
-            this.sock.write(`${this.padLine(n)} ${command}\r\n`)
+        // hack using promise resolve as callback
+        return await new Promise((s, _) => {
+            // correct line number if needed
+            let n = this.line
+            while (this.queue[this.padLine(n)]) n++;
+            // store resolve as callback + send command
+            const line = this.padLine(n)
+            this.queue[line] = s
+            this.sock.write(`${line} ${command}\r\n`)
+            this.line++;
         })
     }
     async exec(cmd, f) {
-        let d = await this.execute(cmd).catch(e => console.log(e))
-        if (!d.match(/^[0-9]+ OK/gim)) throw d
-        return d.match(/^[0-9]+ OK/gim) ? (f ? f(d) : d) : null
-        /*
-        // pass f if you want to give a preprocessor
-        return await new Promise((s, j) => {
-            this.execute(cmd).then((d) => { //d.match(/^0-9]+ OK/gim) ? s(f ? f(d) : d) : null)
-                try {
-                    if (!d.match(/^[0-9]+ OK/gim)) throw d //j(d)
-                    else s(f ? f(d) : d)
-                } catch (e) {
-                    console.log(e)
-                    throw e
-                    //j(e)
-                }
-            })
-        })*/
+        const s = await this.execute(cmd).catch(console.error)
+        const r_ok = /^[0-9]+ OK/gim
+        assert(!!s.match(r_ok), 'Mailserver did not return OK')
+        return f ? f(s) : s;
     }
     async countMessages(box) {
-        return await this.exec(`STATUS ${box} (MESSAGES)`, (d) => eval(d.match(/[0-9]*/g).filter(_ => _)[0]))
+        return await this.exec(`STATUS ${box} (MESSAGES)`, d => {
+            const n = d.match(/[0-9]*/g).filter(_ => _)[0]
+            // TODO: eval is a serious security bug if mailservers act malicious
+            return eval(n)
+        })
     }
     async login(username, password, xoauth) {
         if (password || this.opts.pass)
             return await this.exec(`LOGIN ${username || this.opts.user} ${password || this.opts.pass}`)
         else {
-            //console.log("TRYING XOAUTH");
-            let s = await this.exec(`AUTHENTICATE XOAUTH2 ${xoauth || this.opts.xoauth}`)
+            const s = await this.exec(`AUTHENTICATE XOAUTH2 ${xoauth || this.opts.xoauth}`)
+            // some mailservers get angry if you dont respond empty in next line after XOAUTH
             this.exec(``)
             return s
         }
@@ -89,6 +116,7 @@ class IMAP {
         await this.select(fromFolder)
         await this.exec(`COPY ${uid} ${toFolder}`)
         return await this.exec(`STORE ${uid} +FLAGS \\Deleted`)
+        // we do this without EXPUNGE. if you want to expunge, uncomment below line
         //return await this.exec('EXPUNGE')
     }
     async deleteMessages(uid) {
@@ -116,54 +144,10 @@ class IMAP {
         return await this.exec(`SELECT ${boxName}`)
     }
     async getSenders(start, stop) {
-        return await this.exec(`FETCH ${start || 1}${stop ? ':' + stop : ''} (FLAGS BODY[HEADER.FIELDS (FROM)])`, (d) => Promise.all(
-            d.split(/(?=\* [0-9]* FETCH .*(\r\n|\n))/g)
-            .filter(_ => _.length > 5)
-            .map(async email => {
-                let parser = new MailParser()
-                let s = new Promise((s, j) => parser.on('end', (mail) => s(mail)))
-                parser.write(email)
-                parser.end()
-                let parsedEmail = await s
-                if (parsedEmail.headers && Object.keys(parsedEmail.headers).filter(x => x.indexOf('\\seen') > 0).length > 0)
-                    parsedEmail.headers.seen = true
-                else parsedEmail.headers.seen = false
-                if (parsedEmail.headers && Object.keys(parsedEmail.headers).filter(x => x.indexOf('\\flagged') > 0).length > 0) parsedEmail.headers.starred = true
-                else parsedEmail.headers.starred = false
-                try {
-                    parsedEmail.headers.id = eval(Object.keys(parsedEmail.headers).filter(key => /\* [0-9]* fetch .*/g.test(key))[0].split(' ')[1])
-                } catch(e) {
-                    parsedEmail.headers.id = ('*' != stop ? stop : start)
-                }
-                return parsedEmail
-            })
-        ))
+        return await this.exec(`FETCH ${start || 1}${stop ? ':' + stop : ''} (FLAGS BODY.PEEK[HEADER.FIELDS (FROM)])`, d => Promise.all(process_emails(d, start, stop)))
     }
     async getEmails(start, stop) {
-        return await this.exec(`FETCH ${start || 1}${stop ? ':' + stop : ''} (FLAGS BODY.PEEK[])`, (d) => Promise.all(             d.split(/(?=\* [0-9]* FETCH .*(\r\n|\n))/g)
-            .filter(_ => _.length > 5)
-            .map(async email => {
-                let parser = new MailParser()
-                let s = new Promise((s, j) => parser.on('end', (mail) => s(mail)))
-                parser.write(email)
-                parser.end()
-                let parsedEmail = await s
-                if (parsedEmail.headers && Object.keys(parsedEmail.headers).filter(x => x.indexOf('\\seen') > 0).length > 0)
-                    parsedEmail.headers.seen = true
-                else parsedEmail.headers.seen = false
-                if (parsedEmail.headers && Object.keys(parsedEmail.headers).filter(x => x.indexOf('\\deleted') > 0).length > 0)
-                    parsedEmail.headers.deleted = true
-                else parsedEmail.headers.deleted = false
-                if (parsedEmail.headers && Object.keys(parsedEmail.headers).filter(x => x.indexOf('\\flagged') > 0).length > 0) parsedEmail.headers.starred = true
-                else parsedEmail.headers.starred = false
-                try {
-                    parsedEmail.headers.id = eval(Object.keys(parsedEmail.headers).filter(key => /\* [0-9]* fetch .*/g.test(key))[0].split(' ')[1])
-                } catch(e) {
-                    parsedEmail.headers.id = ('*' != stop ? stop : start)
-                }
-                return parsedEmail
-            })
-        ))
+        return await this.exec(`FETCH ${start || 1}${stop ? ':' + stop : ''} (FLAGS BODY.PEEK[])`, (d) => Promise.all(process_emails(d, start, stop)))
     }
 }
 
--- IMAP/test.js
+++ IMAP/test.js
@@ -2,6 +2,13 @@ const IMAP = require('./conn.js')
 const args = process.argv.slice(2)
 const fetch = require('node-fetch')
 
+console.log({
+    host: args[0],
+    port: args[1],
+    user: args[2],
+    pass: args[3]
+})
+
 let client = new IMAP({
     host: args[0],
     port: args[1],
@@ -11,14 +18,20 @@ let client = new IMAP({
 
 let x = async () => {
     await client.open()
+    console.log("Logging in...")
     await client.login()
+    console.log("Getting folders...")
     let folders = await client.getFolders()
     folders.forEach(_ => console.log('\t' + _))
+    console.log("Selecting inbox...")
     await client.select('INBOX')
+    console.log("Counting messages...")
     let numEmails = await client.countMessages('INBOX')
     console.log(numEmails)
+    console.log("Fetching emails...")
     let emails = await client.getEmails('*', '*')
     console.log(`Got ${emails.length} emails`)
+    console.log("Fetching senders...")
     let senders = await client.getSenders('*', '*')
     console.log(emails[0].headers)
     console.log(senders[0])
